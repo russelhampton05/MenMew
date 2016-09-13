720a95122f64553c87918b4a957c13042f7ee724
russel <russel.hampton@gmail.com>
2016-09-13 10:27:58 -0500

    Finally fully implemented one of the database tables.
    Next up:
    Need to finish implementing the other schema entries.
    Need to make sure I understand sqlites way of doing primary keys, foreign keys, and relations.

--- webApp/Team3/MenMew/urls.py
+++ webApp/Team3/MenMew/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from MenMew import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
-    url(r'^users/$', views.user_list),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- webApp/Team3/MenMew/views.py
+++ webApp/Team3/MenMew/views.py
@@ -1,62 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+
 from MenMew.models import User
 from MenMew.serializers import UserSerializer
+from rest_framework import generics
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
-@csrf_exempt
-def user_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return JSONResponse(serializer.data)
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
 
-@csrf_exempt
-def user_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(user, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        user.delete()
-        return HttpResponse(status=204)
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
