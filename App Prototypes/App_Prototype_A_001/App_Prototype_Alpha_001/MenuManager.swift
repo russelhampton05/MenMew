9e8d6564e4e5365fdf3ed8e783b8dc2492877c62
Russel Hampton <russel@russels-Mac.local>
2016-10-09 11:11:02 -0500

    minor changes to models

---
+++ App Prototypes/App_Prototype_A_001/App_Prototype_Alpha_001/MenuManager.swift
@@ -0,0 +1,19 @@
+//
+//  MenuManager.swift
+//  App_Prototype_Alpha_001
+//
+//  Created by russel w h on 10/7/16.
+//  Copyright © 2016 Jon Calanio. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class MenuManager{
+static let ref = FIRDatabase.database().reference().child("menus")
+    
+    static GetMenu(id: String) -> Menu{
+    
+    }
+}
+
--- App Prototypes/App_Prototype_A_001/App_Prototype_Alpha_001/Models.swift
+++ App Prototypes/App_Prototype_A_001/App_Prototype_Alpha_001/Models.swift
@@ -29,48 +29,63 @@ class Menu{
     var rest_id : String
     var title : String
     var cover_picture : String?
-    var menu_groups : [MenuItem] = []
-    init(id:String, title:String, cover_picture:String?, groups: [MenuItem]){
+    var menu_groups : [MenuGroup] = []
+    init(id:String, title:String, cover_picture:String?, groups: [MenuGroup]){
         self.rest_id = id
         self.title = title
     
         self.cover_picture = cover_picture
         self.menu_groups = groups
     }
-    convenience init(id:String, title:String, groups: [MenuItem] ) {
+    convenience init(id:String, title:String, groups: [MenuGroup] ) {
         self.init(id:id, title:title, cover_picture:nil, groups:groups)
     }
 }
 
+class MenuGroup{
+    var cover_picture: String?
+    var desc: String
+    var title: String?
+    var items: [MenuItem]
+    
+    init(desc: String,items: [MenuItem], title: String?, cover_picture:String?){
+        
+    }
+    convenience init(desc: String,items: [MenuItem], title: String?){
+        self.init(desc:desc,items:items, title:title, cover_picture:nil)
+    }
+    convenience init(desc: String,items: [MenuItem], cover_picture:String?) {
+        self.init(desc:desc,items:items, title:nil, cover_picture:cover_picture)
+    }
+    convenience init(desc: String,items: [MenuItem]) {
+        self.init(desc:desc, items:items, title:nil, cover_picture:nil)
+    }
+}
+//menu item objects and menus are for UI purposes only. 
+//Menu item will have a member called "item", which will tie it in to the actaul
+//details necessary for order tracking.
 
 class MenuItem {
     //will eventually point to an actual item (if we care to implement that, possibly not)
     //for now just UI facing fields and those needed for ordering/pricing
-    enum CookType{
-        case rare
-        case medium
-        case well_done
-    }
-    var cook_type: [CookType]?
+ 
     var title: String
     var price: Double
-    var sides: [String]?
+    //var sides: [String]?
     var image: String
     var desc: String
-    convenience init(title:String, price:Double, image: String, desc: String, sides:[String]?){
-        self.init(title : title, price : price, image : image, desc: desc, sides:sides, cook_type: nil)
-    }
-     init(title:String, price:Double, image: String, desc: String, sides:[String]?,cook_type:[CookType]?) {
-        self.cook_type = cook_type
+   
+     init(title:String, price:Double, image: String, desc: String) {
+      
         self.title = title
         self.image = image
         self.price = price
-        self.sides = sides
+      //  self.sides = sides
         self.desc = desc
     }
-    convenience init(title:String, price:Double, image: String, desc: String, cook_type: [CookType]?){
-        self.init(title : title, price : price, image : image, desc: desc, sides:nil, cook_type: cook_type)
-    }
+    //convenience init(title:String, price:Double, image: String, desc: String){
+       // self.init(title : title, price : price, image : image, desc: desc, sides:nil)
+    //}
 
 }
 
